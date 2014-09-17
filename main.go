d5dbd83747a9b351f1a43bd5b9694145eb3dbb31
Lettergram <Lettergramm@gmail.com>
2014-09-17 09:23:41 -0500

    Refactored and formated main.go

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import(
+import (
 	"fmt"
 	"math/rand"
 	"time"
@@ -8,51 +8,51 @@ import(
 
 func main() {
 
-  slice := generateSlice(50)
-  fmt.Println("\n --- unsorted --- \n\n", slice)
-  fmt.Println("\n--- sorted ---\n\n", MergeSort(slice), "\n")
+	slice := generateSlice(50)
+	fmt.Println("\n --- unsorted --- \n\n", slice)
+	fmt.Println("\n--- sorted ---\n\n", MergeSort(slice), "\n")
 }
 
 // Generates a slice of size, size filled with random numbers
-func generateSlice(size int) []int {	
+func generateSlice(size int) []int {
 
-  slice := make([]int, size, size)
-  rand.Seed(time.Now().UnixNano())
-  for i := 0; i < size; i++ {
-      slice[i] = rand.Intn(99999) - rand.Intn(99999)
-  }
-  return slice
+	slice := make([]int, size, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(99999) - rand.Intn(99999)
+	}
+	return slice
 }
 
 // Runs MergeSort algorithm on a slice single
-func MergeSort(slice []int) []int{
+func MergeSort(slice []int) []int {
 
-	if len(slice) < 2{
+	if len(slice) < 2 {
 		return slice
 	}
-	mid := (len(slice))/2
+	mid := (len(slice)) / 2
 	return Merge(MergeSort(slice[:mid]), MergeSort(slice[mid:]))
 }
 
 // Merges left and right slice into newly created slice
-func Merge(left, right []int)[]int{
-	
-	size, i, j := len(left) + len(right), 0, 0
+func Merge(left, right []int) []int {
+
+	size, i, j := len(left)+len(right), 0, 0
 	slice := make([]int, size, size)
 
 	for k := 0; k < size; k++ {
-		if i > len(left)-1 && j <= len(right)-1{
+		if i > len(left)-1 && j <= len(right)-1 {
 			slice[k] = right[j]
 			j++
-		}else if j > len(right)-1 && i <= len(left)-1{
+		} else if j > len(right)-1 && i <= len(left)-1 {
 			slice[k] = left[i]
 			i++
-		}else if left[i] < right[j] {
+		} else if left[i] < right[j] {
 			slice[k] = left[i]
 			i++
-		}else{
+		} else {
 			slice[k] = right[j]
-			j++	
+			j++
 		}
 	}
 	return slice
